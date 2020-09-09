563e293cd650ce14f6a9d1596b73218f898e75dd
nicolasbelfis <nicolas.belfis@codurance.com>
2020-09-09 19:44:57 +0200

    refactor 1 : remove pattern matching cases

--- src/main/scala/example/RomanLiteral.scala
+++ src/main/scala/example/RomanLiteral.scala
@@ -3,15 +3,10 @@ package example
 object RomanLiteral {
 
 
-  def convert(number: Int): String =  number match {
-    case 1 => "I"
-    case 5 => "V"
-    case 10 => "X"
-    case _ => {
-      if(number >= 10) "X"+convert(number -10)
-      else if(number >= 1) "I"+convert(number -1)
-      else ""
-    }
-  }
+  def convert(number: Int): String =
+  if (number >= 10) "X" + convert(number - 10)
+  else if (number >= 5) "V" + convert(number - 5)
+  else if (number >= 1) "I" + convert(number - 1)
+  else ""
 
 }
