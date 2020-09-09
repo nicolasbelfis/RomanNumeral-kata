2c2a132325a1ca4a5bcc94e7ab1775ef9ede3cca
nicolasbelfis <nicolas.belfis@codurance.com>
2020-09-09 19:59:33 +0200

    refactor 2 : extract constants

--- src/main/scala/example/RomanLiteral.scala
+++ src/main/scala/example/RomanLiteral.scala
@@ -3,10 +3,17 @@ package example
 object RomanLiteral {
 
 
-  def convert(number: Int): String =
-  if (number >= 10) "X" + convert(number - 10)
-  else if (number >= 5) "V" + convert(number - 5)
-  else if (number >= 1) "I" + convert(number - 1)
-  else ""
+  def convert(number: Int): String = {
+    val tenToX = (10,"X")
+    val fiveToV = (5,"V")
+    val oneToI = (1,"I")
+    if (number >= tenToX._1)
+      tenToX._2 + convert(number - tenToX._1)
+    else if (number >= fiveToV._1)
+      fiveToV._2 + convert(number - fiveToV._1)
+    else if (number >= oneToI._1)
+      oneToI._2 + convert(number - oneToI._1)
+    else ""
+  }
 
 }
