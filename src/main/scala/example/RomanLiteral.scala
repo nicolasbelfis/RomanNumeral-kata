3d7ac76b38d60c44be1dc2948ee383acea4a52f1
nicolasbelfis <nicolas.belfis@codurance.com>
2020-09-09 21:16:14 +0200

    refactor 3 : generalize ifs passing roman represntations mapping into the recurion

--- src/main/scala/example/RomanLiteral.scala
+++ src/main/scala/example/RomanLiteral.scala
@@ -2,18 +2,26 @@ package example
 
 object RomanLiteral {
 
-
   def convert(number: Int): String = {
-    val tenToX = (10,"X")
-    val fiveToV = (5,"V")
-    val oneToI = (1,"I")
-    if (number >= tenToX._1)
-      tenToX._2 + convert(number - tenToX._1)
-    else if (number >= fiveToV._1)
-      fiveToV._2 + convert(number - fiveToV._1)
-    else if (number >= oneToI._1)
-      oneToI._2 + convert(number - oneToI._1)
-    else ""
+    iterate(
+      number,
+      List(
+        RomanRepresentation(10, "X"),
+        RomanRepresentation(5, "V"),
+        RomanRepresentation(1, "I")
+      ))
   }
 
+  private def iterate(number: Int,
+                      representations: List[RomanRepresentation]
+                     ): String =
+    representations match {
+      case h :: t =>
+        if (number >= h.arabic)
+          h.roman + iterate(number - h.arabic, h :: t)
+        else iterate(number, t)
+      case Nil => ""
+    }
 }
+
+case class RomanRepresentation(arabic: Int, roman: String)
